ae8abbb14aa019d7987256067cb2ae53831f0ab1
Matt Kubej <mkubej@gmail.com>
2020-07-25 14:22:21 -0400

    feat: start introducing decode array

--- src/resp-decoder.ts
+++ src/resp-decoder.ts
@@ -1,33 +1,37 @@
-const CR = '\r';
-const LF = '\n';
+const CR = '\\r';
+const LF = '\\n';
 const CRLF = `${CR}${LF}`;
 
 enum RESPType {
   SimpleString = '+',
   Error = '-',
-  Integer = '',
+  Integer = ':',
   BulkString = '$',
   Array = '*',
 };
 
 export function decode(value: Buffer): string {
-  const input = value.toString('utf8');
+  const input = value.toString('utf8').trim();
+  console.log(input);
   if (!input.endsWith(CRLF)) throw new Error('Incomplete command'); 
 
   const first = input[0];
+  const bytes = input.substring(1);
+
   switch(first) {
     case RESPType.SimpleString:
-      return decodeSimpleString(input);
+      return decodeSimpleString(bytes);
     case RESPType.BulkString:
-      return decodeBulkString(input);
+      return decodeBulkString(bytes);
+    case RESPType.Array:
+      return decodeArray(bytes);
     default:
       throw new Error(`Unhandled first char: ${first}`);
   }
-
 };
 
 function decodeSimpleString(input: string): string {
-  const simpleString = input.slice(1, -2);
+  const simpleString = input.slice(0, -4); //TODO: test this
 
   if (simpleString.includes(CR)) 
     throw new Error('Simple Strings cannot contain a CR');
@@ -36,16 +40,27 @@ function decodeSimpleString(input: string): string {
     throw new Error('Simple Strings cannot contain a LF');
 
   return simpleString;
-}
+};
 
 function decodeBulkString(input: string): string {
-  const bytes = Number(input[1]);
-  if (input.substr(2, 2) !== CRLF)
+  const bytes = Number(input[0]); //TODO: only handles single digit
+  if (input.substr(1, 4) !== CRLF)
     throw new Error('Bulk String bytes must be terminated with CRLF');
 
-  const bulkString = input.substr(4, bytes);
+  const bulkString = input.substr(5, bytes); //TODO: need to accommodate length of bytes
   if (bulkString.length !== bytes)
     throw new Error('Bulk String bytes does not match string length');
 
   return bulkString;
+};
+
+function decodeArray(input: string): string {
+  let bytes = Number(input[0]); //TODO: only handles single digit
+
+  while (bytes > 0) {
+    const d = decode(Buffer.from(input.substr(5)));
+    bytes--;
+  }
+
+  return input;
 }
