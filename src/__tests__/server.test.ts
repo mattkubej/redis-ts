0e19737e1a2b3e956cc87cbc09762237b5dbcbd5
Matt Kubej <mkubej@gmail.com>
2020-08-09 14:26:40 -0400

    test: introduce test for get command

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -3,28 +3,19 @@ import { createConnection, Server, Socket } from 'net';
 import * as cmd from '../cmd';
 
 jest.mock('../cmd/echo');
+jest.mock('../cmd/get');
 
 describe('server', () => {
   let redisServer: RedisServer;
   let server: Server;
   let client: Socket;
+  let request: Promise<Buffer>;
 
-  beforeAll(() => {
+  beforeEach(async () => {
     redisServer = new RedisServer();
     server = redisServer.listen(3999);
-  });
 
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  afterAll(() => {
-    client.destroy();
-    server.close();
-  });
-
-  it('should invoke the echo command with a echo request', async () => {
-    const request = new Promise((resolve) => {
+    request = new Promise((resolve) => {
       server.on('connection', (socket) => {
         socket.on('data', resolve);
       });
@@ -35,8 +26,16 @@ describe('server', () => {
         resolve(c);
       });
     });
+  });
 
-    client.write(Buffer.from('*1\r\n$4\r\nECHO\r\n'));
+  afterEach(() => {
+    client.destroy();
+    server.close();
+    jest.clearAllMocks();
+  });
+
+  it('should invoke the echo command with a echo request', async () => {
+    client.write(Buffer.from('*2\r\n$4\r\nECHO\r\n$4\r\ntest\r\n'));
     await request;
 
     const mockExecute = (cmd.Echo as jest.Mock).mock.instances[0].execute.mock;
@@ -45,6 +44,19 @@ describe('server', () => {
 
     expect(executeClient instanceof Socket).toBeTruthy();
     expect(executeClient.server).toBe(server);
-    expect(executeRequest).toStrictEqual(['ECHO']);
+    expect(executeRequest).toStrictEqual(['ECHO', 'test']);
+  });
+
+  it('should invoke the get command with a get request', async () => {
+    client.write(Buffer.from('*2\r\n$3\r\nGET\r\n$4\r\ntest\r\n'));
+    await request;
+
+    const mockExecute = (cmd.Get as jest.Mock).mock.instances[0].execute.mock;
+    const executeClient = mockExecute.calls[0][0];
+    const executeRequest = mockExecute.calls[0][1];
+
+    expect(executeClient instanceof Socket).toBeTruthy();
+    expect(executeClient.server).toBe(server);
+    expect(executeRequest).toStrictEqual(['GET', 'test']);
   });
 });
