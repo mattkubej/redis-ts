61e406d07e3f0c543b677e6d3030b6409094494e
Matt Kubej <mkubej@gmail.com>
2020-08-10 21:46:42 -0400

    test: introduce tests for decoder

---
+++ src/resp/__tests__/decoder.test.ts
@@ -0,0 +1,44 @@
+import { decode } from '../decoder';
+
+describe('decode', () => {
+  describe('simple string', () => {
+    it('should decode an encoded simple string to a string', () => {
+      const data = Buffer.from('+TEST\r\n');
+      const result = decode(data);
+      expect(result).toStrictEqual(['TEST']);
+    });
+  });
+
+  describe('bulk string', () => {
+    it('should decode an encoded bulk string to a string', () => {
+      const data = Buffer.from('$4\r\nTEST\r\n');
+      const result = decode(data);
+      expect(result).toStrictEqual(['TEST']);
+    });
+  });
+
+  describe('integer', () => {
+    it('should decode an encoded integer to a number', () => {
+      const data = Buffer.from(':2\r\n');
+      const result = decode(data);
+      expect(result).toStrictEqual([2]);
+    });
+  });
+
+  describe('array', () => {
+    it('should decode an encoded array to array', () => {
+      const data = Buffer.from('*2\r\n+TEST\r\n*2\r\n$4\r\nABCD\r\n:2\r\n');
+      const result = decode(data);
+      expect(result).toStrictEqual(['TEST', ['ABCD', 2]]);
+    });
+  });
+
+  describe('error', () => {
+    it('should decode an encoded error to a thrown error', () => {
+      expect(() => {
+        const data = Buffer.from('-BOOM\r\n');
+        decode(data);
+      }).toThrowError('BOOM');
+    });
+  });
+});
