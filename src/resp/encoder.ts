6e7b831744cb04f87c3c24aaf3309aceabd332d7
Matt Kubej <mkubej@gmail.com>
2020-08-13 21:29:15 -0400

    refactor: utilize resp constants in encoder

--- src/resp/encoder.ts
+++ src/resp/encoder.ts
@@ -1,22 +1,23 @@
-// TODO: utilize constants
+import { CRLF, RESPType } from './constants';
+
 export function encodeSimpleString(value: string): string {
-  return `+${value}\r\n`;
+  return `${RESPType.SimpleString}${value}${CRLF}`;
 }
 
 export function encodeBulkString(value: string): string {
-  return `$${value.length}\r\n${value}\r\n`;
+  return `${RESPType.BulkString}${value.length}${CRLF}${value}${CRLF}`;
 }
 
 export function encodeInteger(value: number): string {
-  return `:${value}\r\n`;
+  return `${RESPType.Integer}${value}${CRLF}`;
 }
 
 export function encodeArray(values: string[]): string {
   return values.reduce(function (acc, cur) {
     return (acc += cur);
-  }, `*${values.length}\r\n`);
+  }, `${RESPType.Array}${values.length}${CRLF}`);
 }
 
 export function encodeError(value: string): string {
-  return `-ERR ${value}\r\n`;
+  return `${RESPType.Error}ERR ${value}${CRLF}`;
 }
