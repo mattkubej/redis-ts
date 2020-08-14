95b8a3a77dc8839a2bbdee47b9148a44497d21ef
Matt Kubej <mkubej@gmail.com>
2020-08-14 00:00:35 -0400

    docs: add reminder to introduce PORT env var

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import RedisServer from './server';
 
 const redisServer = new RedisServer();
 
+// TODO: introduce environment variable to change port
 redisServer.listen(6379, function () {
   console.log('listening on 6379');
 });
