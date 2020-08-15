4682ecf4c1771984463dc845bbd50c1b6289d911
Matt Kubej <mkubej@gmail.com>
2020-08-15 11:56:17 -0400

    feat: ability to define port with env var

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import RedisServer from './server';
 
 const redisServer = new RedisServer();
+const port = parseInt(process.env.PORT, 10) || 6379;
 
-// TODO: introduce environment variable to change port
-redisServer.listen(6379, function () {
-  console.log('listening on 6379');
+redisServer.listen(port, function () {
+  console.log(`listening on ${port}`);
 });
