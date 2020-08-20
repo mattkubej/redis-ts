df68d571f789bcb24e371fd0c1303783971d2c52
Matt Kubej <mkubej@gmail.com>
2020-08-20 00:04:45 -0400

    docs: validated port env var worked

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import RedisServer from './server';
 
 const redisServer = new RedisServer();
-
-// TODO: test this
 const port = parseInt(String(process.env.PORT), 10) || 6379;
 
 redisServer.listen(port, function () {
