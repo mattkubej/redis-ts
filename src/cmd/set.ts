6037ce3e8321271d68e1ce841b4ee0e88e124172
Matt Kubej <mkubej@gmail.com>
2020-08-21 14:43:54 -0400

    refactor: clean up implementation of set command

--- src/cmd/set.ts
+++ src/cmd/set.ts
@@ -9,31 +9,23 @@ export default class Set extends RedisCommand {
     super('set', -3, ['write', 'denyoom'], 1, 1, 1);
   }
 
-  // TODO: clean this up
   execute(client: Socket, request: Data[]): void {
     const key = String(request[1]);
     const value = String(request[2]);
     const option = request[3] ? String(request[3]).toLowerCase() : undefined;
 
-    if (option === undefined) {
+    if (
+      option === undefined ||
+      (option === 'nx' && get(key) === undefined) ||
+      (option === 'xx' && get(key) !== undefined)
+    ) {
       set(key, value);
       client.write(OK);
-    } else if (option === 'nx') {
-      // set if key does not exist
-      if (get(key) === undefined) {
-        set(key, value);
-        client.write(OK);
-      } else {
-        client.write(NULL);
-      }
-    } else if (option === 'xx') {
-      // set if key already exists
-      if (get(key) === undefined) {
-        client.write(NULL);
-      } else {
-        set(key, value);
-        client.write(OK);
-      }
+    } else if (
+      (option === 'nx' && get(key) !== undefined) ||
+      (option === 'xx' && get(key) === undefined)
+    ) {
+      client.write(NULL);
     } else {
       throw new Error('syntax error');
     }
